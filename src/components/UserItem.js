3249af729cda177d977189eb732ea315666bb4b5
dkrefta <diego.krefta@gmail.com>
2018-04-03 21:26:24 -0300

    Add Spinners + Update CSS

--- src/components/UserItem.js
+++ src/components/UserItem.js
@@ -9,18 +9,17 @@ render () {
     <div className="card-content">
     <div className="UserItem-leftBox">
     <img
+    className="UserItem-image"
     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo3lObwTn2QuoCHSM_wMN91LuErjKsAeOgKnK0gVhDMTNqoTUt"
     alt="profile"/>
     </div>
-    <div clasName="UserItem-rightBox">
-     <h2 className="UserItem-name"> {name} </h2>
-     <h3 className="UserItem-last_name"> {last_name} </h3>
+    <div className="UserItem-rightBox">
+     <h2 className="UserItem-name"> {name + " " + last_name} </h2>
      <h3 className="UserItem-facebook"> {facebook} </h3>
-     //<h3 className="UserItem-id"> {id} </h3>
-    </div>
-    </div>
-    </div>
-  );
+          </div>
+        </div>
+      </div>
+    );
 }
 
 }
